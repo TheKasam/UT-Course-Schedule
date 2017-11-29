f4c0670a777290c204900b8e89e97f0b27cda9d2
Sai Kasam <saikasam98@gmail.com>
2017-11-29 14:30:53 -0600

    think i merged test2 into script

--- script.py
+++ script.py
@@ -1,13 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-import requests
 import re
-from robobrowser import RoboBrowser
+import mechanicalsoup
+import bs4 as bs
 
 #firebase-adminsdk-jgy6n@courseschedule-8a816.iam.gserviceaccount.com
-
-
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('courseschedule-8a816-firebase-adminsdk-jgy6n-2f35d9eaad.json')
 
@@ -17,8 +15,43 @@ firebase_admin.initialize_app(cred, {
 })
 
 
+def main():
+
+    with open("soup.html") as f:
+      soup = bs.BeautifulSoup(f,'lxml')
 
 
-db.reference().child('courses').child('1').update({
-'name':'bob'
-})
+    #print(saveUnique('40350',soup))
+
+
+#gets data about course and saves it
+def saveUnique(unique,soup):
+
+    course_a = soup.find(text=unique).parent
+    course_tr = course_a.parent.parent
+
+    days = course_tr.find('td',{'data-th':'Days'}).select_one('span').text
+    hour = course_tr.find('td',{'data-th':'Hour'}).select_one('span').text
+    room = course_tr.find('td',{'data-th':'Room'}).select_one('span').text
+    instructor = course_tr.find('td',{'data-th':'Instructor'}).text
+    status = course_tr.find('td',{'data-th':'Status'}).text
+
+
+    db.reference().child('courses').child('1').update({
+        'days':days,
+        'hour':hour,
+        'room':room,
+        'instructor':instructor,
+        'status':status,
+    })
+    return(status)
+
+
+def findHeaders():
+    headers = []
+    for header in soup.find_all('td',{'class':'course_header'}):
+        headers.append(header)
+    return headers
+
+
+main()
--- test2.py
+++ test2.py
@@ -2,21 +2,44 @@ import re
 import mechanicalsoup
 import bs4 as bs
 
+#regular query: course_tr.find('td',{'data-th':'Days'})
+#text gives you string
+#contents gives you list
+#select_one
+#find text     course_a = soup.find(text=unique).parent
+#getting class value - b.get('class')
+
+def main():
+
+    with open("soup.html") as f:
+      soup = bs.BeautifulSoup(f,'lxml')
 
 
-with open("soup.html") as f:
-  soup = bs.BeautifulSoup(f,'lxml')
+    u = soup.find(text="40350").parent
 
-
-u = soup.find('a',{'title':"Unique number"})
-b = u.parent.parent
-print(b.get('class'))
-
-def fineUnique():
-    pass
+    print(findUnique('40350',soup))
 
 
 
+def findUnique(unique,soup):
+
+    course_a = soup.find(text=unique).parent
+    course_tr = course_a.parent.parent
+
+    days = course_tr.find('td',{'data-th':'Days'}).select_one('span').text
+    hour = course_tr.find('td',{'data-th':'Hour'}).select_one('span').text
+    room = course_tr.find('td',{'data-th':'Room'}).select_one('span').text
+    instructor = course_tr.find('td',{'data-th':'Instructor'}).text
+    status = course_tr.find('td',{'data-th':'Status'}).text
+    return(status)
+
+    db.reference().child('courses').child('unique').child('1').update({
+        'days':days,
+        'hour':hour,
+        'room':room,
+        'instructor':instructor,
+        'status':status,
+    })
 
 
 def findHeaders():
@@ -24,3 +47,6 @@ def findHeaders():
     for header in soup.find_all('td',{'class':'course_header'}):
         headers.append(header)
     return headers
+
+
+main()
