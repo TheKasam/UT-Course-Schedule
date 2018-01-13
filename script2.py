32cc74636d32d6205d98198a2d0772f138d47147
Sai Kasam <saikasam98@gmail.com>
2018-01-13 15:46:12 -0600

    test script for pages

--- script2.py
+++ script2.py
@@ -20,7 +20,7 @@ def main():
 
 
 
-    feild = "bio"
+    feild = "mis"
     number = '325'
     browser.open("https://utdirect.utexas.edu/apps/registrar/course_schedule/20182/results/?ccyys=20182&search_type_main=COURSE&fos_cn=" + feild+ "&course_number="+number)
     soup = browser.get_current_page()
@@ -31,5 +31,5 @@ def main():
     soup = browser.get_current_page()
     course_a = soup.find('td',{'class':'course_header'})
     b = soup.find_all('a',{'title':'Unique number'})
-    print(b)
+
 main()
