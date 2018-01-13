315cd586f0a33992858dca81c53d0b05374f9138
Sai Kasam <saikasam98@gmail.com>
2018-01-13 15:40:21 -0600

    test script for pages

--- script2.py
+++ script2.py
@@ -1,13 +1,31 @@
-import sys
-import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import bs4 as bs
 import re
 import mechanicalsoup
+import time
 import pickle
+import sys
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+from email.mime.text import MIMEText
+sys.setrecursionlimit(50000)
 
 
-browser = mechanicalsoup.StatefulBrowser()
-browser.open("https://www.google.com/")
-soup = browser.get_current_page()
+def main():
+    with open("browser.pickle","rb") as f:
+        browser = pickle.load(f)
+        checkBrowserBool = checkBrowser(browser)
 
-with open("sp500tickers.pickle","wb") as f:
-    pickle.dump(browser,f)
+
+    feild = "bio"
+    number = 325
+    browser.open("https://utdirect.utexas.edu/apps/registrar/course_schedule/20182/results/?ccyys=20182&search_type_main=COURSE&fos_cn=" + feild+ "&course_number="+number)
+    soup = browser.get_current_page()
+
+    nextPageUrl = soup.find('a',{'id':'next_nav_link'})
+    print(nextPageUrl.text)
+
+main()
