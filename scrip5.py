e9cabfa54af0115a177500123d173c28628d4db6
Venu Kasam <Venu@Venus-MacBook-Pro.local>
2018-01-17 01:02:57 -0600

    script5

--- scrip5.py
+++ scrip5.py
@@ -22,7 +22,41 @@ def main():
 
 main()
 
+def logIn():
+    # Connect to Google
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open("https://utdirect.utexas.edu/apps/registrar/course_schedule/20182/")
 
+    # Fill-in the form
+    browser.select_form('form[name="Login"]')
+    #browser.get_current_form().print_summary()
+    browser["IDToken1"] = "mj27879"
+    browser["IDToken2"] = "Touchstone789"
+
+    browser.submit_selected(btnName="Login.Submit")
+
+    stall(8)
+
+    browser.select_form('form[name="Response"]')
+    browser.submit_selected()
+
+    stall(8)
+
+    browser.select_form('form[name="getform"]')
+    browser.submit_selected()
+
+    stall(8)
+    with open("browser.pickle","wb") as f:
+        pickle.dump(browser,f)
+    #browser.open("https://utdirect.utexas.edu/apps/registrar/course_schedule/20182/results/?ccyys=20182&search_type_main=INSTR&instr_last_name=CONLEY&instr_first_initial=&x=40&y=9")
+    return(browser)
+
+def stall(sec):
+    start = time.time()
+    end = time.time()
+    while end - start < sec:
+        end = time.time()
+        
 def checkBrowser(browser):
 
     ### GET SOUP ###
